5ebc6ab49781dd6a902bb25d0b290c3c2f36e7bf
Micaela Bray <micaela.bray@gmail.com>
2019-07-27 18:13:16 -0500

    Initial Commit and sets up friends.js array

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FriendFinder
 Vanderbilt Bootcamp Homework: Week 13 - FriendFinder
+
+Purpose:
+To match with friends based on responses given to a survey.
---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+    
+var friendsArray = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [4, 3, 2, 4, 2, 2, 3, 2, 5, 4]
+    },
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [2, 1, 2, 1, 2, 2, 3, 4, 1, 2]
+    },
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [1, 3, 4, 4, 4, 5, 2, 2, 2, 2]
+    },
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [2, 5, 2, 1, 4, 3, 5, 2, 2, 1]
+    },
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 4, 4, 5, 5, 5, 4, 3, 2, 5]
+    },
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [5, 1, 4, 4, 5, 2, 3, 4, 2, 3]
+    },
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [3, 2, 4, 3, 2, 1, 5, 5, 3, 2]
+    },
+];
+
+module.exports = friendsArray;
